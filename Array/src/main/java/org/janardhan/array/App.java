2a389249920b19a7626f0f9f55a30e306a231069
Janardhan Maithil <maithiljanardan@gmail.com>
2017-11-03 09:50:27 +0530

    First Commit

---
+++ Array/src/main/java/org/janardhan/array/App.java
@@ -0,0 +1,13 @@
+package org.janardhan.array;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("This is a simple program");
+    }
+}
