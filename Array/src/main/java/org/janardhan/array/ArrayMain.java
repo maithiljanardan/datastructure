ea493acd03efd09fdab06ba0cd2e02fb2891d918
Janardhan Maithil <maithiljanardan@gmail.com>
2017-11-06 23:14:58 +0530

    Added method comments to leftRotateArray and gCd methods in ArrayMain class

--- Array/src/main/java/org/janardhan/array/ArrayMain.java
+++ Array/src/main/java/org/janardhan/array/ArrayMain.java
@@ -6,12 +6,28 @@ package org.janardhan.array;
  */
 public class ArrayMain {
 
+	/**
+	 * This methods left rotates the array by shiftBy positions.
+	 * 
+	 * The inner loop increments in steps of d, and stops when it gets back to the
+	 * starting point, i.e. a total span which is some multiple of n. That multiple
+	 * is LCM(n, d). Thus the number of elements in that cycle is LCM(n, d) / d. The
+	 * total number of such cycles is n / (LCM(n, d) / d), which is equal to GCD(n,
+	 * d).
+	 * 
+	 * @param arr
+	 * @param shiftBy
+	 * @author janardhan 2017
+	 */
 	public static void leftRotateArray(int[] arr, int shiftBy) {
 
 		int arraySize = arr.length;
 
 		int j, k, temp;
 
+		/*
+		 
+		 */
 		for (int i = 0; i < gCd(arraySize, shiftBy); i++) {
 
 			temp = arr[i];
@@ -32,6 +48,14 @@ public class ArrayMain {
 		}
 	}
 
+	/**
+	 * This method calculates the gcd for two given input numbers
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 * @author janardhan 2017
+	 */
 	public static int gCd(int x, int y) {
 		if (y == 0) {
 			return x;
