7c6f87788714e56ba46cd499e90fb8e17a6d763b
Janardhan Maithil <maithiljanardan@gmail.com>
2017-11-30 19:30:07 +0530

    Adds Node class for Binary Tree Implementation

---
+++ Node/src/main/java/org/janardhan/node/Node.java
@@ -0,0 +1,16 @@
+package org.janardhan.node;
+
+public class Node<T extends Comparable<T>> {
+
+	private T data;
+	private Node<T> next;
+
+	public Node(T data, Node<T> next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	public T getData() {
+		return data;
+	}
+}
